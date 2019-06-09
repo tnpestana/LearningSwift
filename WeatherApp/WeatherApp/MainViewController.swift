9581a81f50d237dc14939b5cdf04359feb3f5fbe
Tiago Pestana <tnevespestana@gmail.com>
2019-06-09 16:54:03 +0100

    added CocoaPods

--- WeatherApp/WeatherApp/MainViewController.swift
+++ WeatherApp/WeatherApp/MainViewController.swift
@@ -10,18 +10,17 @@ import UIKit
 
 class MainViewController: UIViewController
 {
-    
     @IBOutlet weak var lblTemperature: UILabel!
     @IBOutlet weak var lblLocation: UILabel!
-    @IBOutlet weak var imgTemperature: UIImageView!
+    @IBOutlet weak var imgWeather: UIImageView!
     
+    let apikey = "8c3536992559ac53a6323e6f7efcc2cc"
+    let endpoint = "http://api.openweathermap.org"
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
