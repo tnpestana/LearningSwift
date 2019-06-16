3704a96462bba070c8a5e2f19f3a39e077e356ec
Tiago Pestana <tnevespestana@gmail.com>
2019-06-16 15:58:39 +0100

    setup chat app to use firebase

---
+++ ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Tiago Pestana on 16/06/2019.
+//  Copyright © 2019 Tiago Pestana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
