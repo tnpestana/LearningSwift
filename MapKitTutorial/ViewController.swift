be6d53aed7bce6e8c8599b23ef110d0906977400
Tiago Pestana <tnevespestana@gmail.com>
2019-07-23 19:50:27 +0100

    enable location services, ask for permission to use location and show users location

--- MapKitTutorial/ViewController.swift
+++ MapKitTutorial/ViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkLocationServices()
     }
-
-
+    
+    func setupLocationManager()
+    {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func checkLocationAuthorization()
+    {
+        switch CLLocationManager.authorizationStatus()
+        {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+        case .denied:
+            // prompt user to turn on location permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        default:
+            break
+        }
+    }
+    
+    func checkLocationServices()
+    {
+        if CLLocationManager.locationServicesEnabled()
+        {
+            setupLocationManager()
+            checkLocationAuthorization()
+        }
+        else
+        {
+            // prompt user to turn on location permissions
+        }
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate
+{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus)
+    {
+    }
+}
