721d24abff83e88d93219d87115722b4e40c6f35
Tiago Pestana <tnevespestana@gmail.com>
2019-07-28 19:37:10 +0100

    add filters array associated with collection view cells

---
+++ CoreImageTutorial/Controller/FilterCell.swift
@@ -0,0 +1,15 @@
+//
+//  FilterCell.swift
+//  CoreImageTutorial
+//
+//  Created by Tiago Pestana on 28/07/2019.
+//  Copyright © 2019 Tiago Pestana. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UICollectionViewCell
+{
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+}
--- CoreImageTutorial/Controller/PreviewViewController.swift
+++ CoreImageTutorial/Controller/PreviewViewController.swift
@@ -12,18 +12,23 @@ import AVFoundation
 class PreviewViewController: UIViewController
 {
     @IBOutlet weak var imagePreview: UIImageView!
+    @IBOutlet weak var filtersCollection: UICollectionView!
     
+    var presentedImage: UIImage?
     var imagePicker: UIImagePickerController!
     var captureSession: AVCaptureSession?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var filters: [String] = CIFilter.filterNames(inCategory: "CICategoryColorEffect")
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        setupPreview()
+        filtersCollection.delegate = self
+        filtersCollection.dataSource = self
+        presentImagePicker()
     }
     
-    func setupPreview()
+    func presentImagePicker()
     {
         imagePicker = UIImagePickerController()
         imagePicker.delegate = self
@@ -32,34 +37,61 @@ class PreviewViewController: UIViewController
         present(imagePicker, animated: true, completion: nil)
     }
     
-    func applyFilter(image: UIImage) -> CIImage?
+    func applyFilter(name: String, image: UIImage) -> CIImage?
     {
-        //let context = CIContext()
         guard let originalCIImage = CIImage(image: image) else { return CIImage() }
-        return sepiaFilter(originalCIImage, intensity: 1.0)
-    }
-    
-    func sepiaFilter(_ input: CIImage, intensity: Double) -> CIImage?
-    {
-        let sepiaFilter = CIFilter(name:"CISepiaTone")
-        sepiaFilter?.setValue(input, forKey: kCIInputImageKey)
-        sepiaFilter?.setValue(intensity, forKey: kCIInputIntensityKey)
-        return sepiaFilter?.outputImage
+        let filter = CIFilter(name: name)
+        filter?.setValue(originalCIImage, forKey: kCIInputImageKey)
+        //filter?.setValue(1.0, forKey: kCIInputIntensityKey)
+        return filter?.outputImage
     }
 }
 
-extension PreviewViewController: UIImagePickerControllerDelegate
+extension PreviewViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
     {
         let chosenImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        presentedImage = chosenImage
         imagePreview.contentMode = .scaleAspectFit
-        imagePreview.image = UIImage(ciImage: applyFilter(image: chosenImage)!)
+        imagePreview.image = presentedImage
+        filtersCollection.reloadData()
         dismiss(animated: true, completion: nil)
     }
 }
 
-extension PreviewViewController: UINavigationControllerDelegate
+extension PreviewViewController: UICollectionViewDelegate, UICollectionViewDataSource
 {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return filters.count + 1
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "filterCell", for: indexPath) as! FilterCell
+        if indexPath.row == 0
+        {
+            cell.imgView.image = presentedImage
+            cell.titleLbl.text = "Original"
+        }
+        else
+        {
+            if let image = applyFilter(name: filters[indexPath.row - 1], image: presentedImage ?? UIImage())
+            {
+                cell.imgView.image = UIImage(ciImage: image)
+            }
+            else
+            {
+                cell.imgView.image = UIImage(named: "default_img")
+            }
+            cell.titleLbl.text = filters[indexPath.row - 1]
+        }
+        return cell
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+//    {
+//        imagePreview.image =
+//    }
 }
