5f35d8ac689c01db9e0208faabee36ecf22fb0ce
Tiago Pestana <tnevespestana@gmail.com>
2019-11-18 20:24:11 +0000

    play and pause local file

--- iOSAudioPlayer/iOSAudioPlayer/ViewController.swift
+++ iOSAudioPlayer/iOSAudioPlayer/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnPlay: UIButton!
+    @IBOutlet weak var btnPause: UIButton!
+    
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnPlayTapped(_ sender: Any) {
+        if let audioPlayer = audioPlayer, audioPlayer.isPlaying { audioPlayer.stop() }
+        
+        guard let path = Bundle.main.path(forResource: "Untitled", ofType: "mp3") else {
+            print("file not found")
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("couldn't load file")
+        }
+    }
+    
+    @IBAction func btnPauseTapped(_ sender: Any) {
+        audioPlayer?.pause()
+    }
 }
 
