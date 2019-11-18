1554972d648a9875d3fd2bb86594535c068c7c9f
Tiago Pestana <tnevespestana@gmail.com>
2019-11-18 19:36:48 +0000

    replace document based template for single app

---
+++ iOSAudioPlayer/iOSAudioPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAudioPlayer
+//
+//  Created by Tiago Pestana on 18/11/2019.
+//  Copyright © 2019 Tiago Pestana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
