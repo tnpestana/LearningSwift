bcad18dbe547a343d065cf4947eb894d42277a89
Tiago Pestana <tnevespestana@gmail.com>
2020-01-03 18:08:16 +0000

    add keyboard view

--- AudioKitTuto/AudioKitTuto/ViewController.swift
+++ AudioKitTuto/AudioKitTuto/ViewController.swift
@@ -11,12 +11,31 @@ import AudioKit
 import AudioKitUI
 
 class ViewController: UIViewController {
-
+    var vKeyboard: AKKeyboardView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadKeyboard()
     }
-
-
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        updateKeyboardFrame(CGRect(size: size))
+    }
+    
+    func loadKeyboard() {
+        if vKeyboard == nil {
+            vKeyboard = AKKeyboardView(frame: CGRect(x: 0, y: UIScreen.main.bounds.height
+                / 2, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2))
+            self.view.addSubview(vKeyboard)
+        }
+    }
+    
+    func updateKeyboardFrame(_ rect: CGRect) {
+        if vKeyboard != nil {
+            vKeyboard.frame = CGRect(x: 0, y: rect.height
+                / 2, width: rect.width, height: rect.height / 2)
+        }
+    }
 }
 
