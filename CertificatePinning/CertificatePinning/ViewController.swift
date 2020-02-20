45c49539fdf501c54745f9b368c2013b1987005c
Tiago Pestana <tnevespestana@gmail.com>
2020-02-20 19:50:13 +0000

    initial commit - create project

---
+++ CertificatePinning/CertificatePinning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CertificatePinning
+//
+//  Created by Tiago Pestana on 20/02/2020.
+//  Copyright © 2020 Tiago Pestana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
